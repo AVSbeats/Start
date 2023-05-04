5717d9f0f50493642de42ba94d4a13bc9bed81ed
Abraham Vallejos-Soto <125098951+AVSbeats@users.noreply.github.com>
2023-05-04 10:14:04 -0400

    First to main

---
+++ Start.py
@@ -0,0 +1,31 @@
+# Variables & Methods Practice
+name = "Abraham Vallejos-Soto"
+print(name.upper())
+print(name.replace("-", " "))
+print("")
+
+# Math Practice
+x = 10 + 5
+x += 5
+print(x)
+print("")
+
+# Booleans 
+price = 25
+print(price > 15 and price < 40)
+print( price > 40 or price < 30)
+print("This will make a true to false like the !=")
+print (price > 20)
+print (not price > 20)
+print("")
+
+# If statements Practice
+temperature = 70
+if temperature > 80:
+    print("It is a hot day")
+elif temperature > 60: # elif is the same as (else if)
+    print("Is perfect my guy")
+else:
+    print("Non of it matters lmao")
+
+# 
